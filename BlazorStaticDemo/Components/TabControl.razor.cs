b0c133b454adb39547dccf9da0fe3432b5298006
Bert Tammenga <btammenga@pcugroup.com>
2022-03-18 12:17:45 +0100

    Added TabControl Test

---
+++ BlazorStaticDemo/Components/TabControl.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorStaticDemo.Components;
+
+partial class TabControl
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    internal TabPage? ActivePage { get; private set; }
+    internal void NewPage(TabPage page)
+    {
+        // optie: pagina's opslaan
+
+        if (ActivePage == null)
+        {
+            ActivatePage(page);
+        }
+    }
+
+    internal void ActivatePage(TabPage page)
+    {
+        ActivePage = page;
+        StateHasChanged();
+    }
+}
---
+++ BlazorStaticDemo/Components/TabPage.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorStaticDemo.Components;
+
+partial class TabPage
+{
+    [CascadingParameter]
+    public TabControl Owner { get; set; } = null!;
+
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+
+    string? IsActiveClass => (this == Owner.ActivePage) ? "active" : null;
+
+    protected override void OnInitialized()
+    {
+        if (Owner == null)
+        {
+            throw new InvalidOperationException("Een TabPage moet op een TabControl staan");
+        }
+        Owner.NewPage(this);
+    }
+
+    private void DoClick()
+    {
+        Owner.ActivatePage(this);
+    }
+}
