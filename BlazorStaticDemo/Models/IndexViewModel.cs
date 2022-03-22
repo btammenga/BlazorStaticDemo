9edb93614841d31bd853a4ce91d1f1782f1cbf00
Bert Tammenga <btammenga@pcugroup.com>
2022-03-22 21:03:22 +0100

    Added ControlGallery

---
+++ BlazorStaticDemo/Models/IndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorStaticDemo.Models;
+
+public class IndexViewModel
+{
+    public string SalesPerson { get; set; } = string.Empty;
+    public double Amount { get; set; }
+    public bool IsActive { get; set; }
+    public List<Location> Locations { get; set; } = new();
+}
---
+++ BlazorStaticDemo/Models/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorStaticDemo.Models;
+
+public class Location
+{
+    public int Id { get; set; }
+    public string City { get; set; }
+    public string ProvState { get; set; }
+    public string Country { get; set; }
+}
