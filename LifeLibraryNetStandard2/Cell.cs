ff9fe97bc22f5f8efea44459d1c510211ba362ca
Bert Tammenga <btammenga@pcugroup.com>
2022-03-28 08:23:38 +0200

    wip

---
+++ LifeLibraryNetStandard2/Cell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeLibraryNetStandard2
+
+{
+    public class Cell
+    {
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+        public bool IsAlive { get; set; }
+
+        public int LivingNeigbours { get; set; }
+        public List<Cell> Neighbours { get; set; }
+
+    }
+}
---
+++ LifeLibraryNetStandard2/HeartbeatEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeLibraryNetStandard2
+{
+    public class HeartbeatEventArgs : EventArgs
+    {
+        public HeartbeatEventArgs(int heartbeatTick, DateTime startTime)
+        {
+            HeartBeatTick = heartbeatTick;
+            StartLiving = startTime;
+            AverageTickRate = (int)Math.Round((DateTime.Now - StartLiving).TotalMilliseconds / HeartBeatTick, 0);
+        }
+        public int HeartBeatTick { get; private set; }
+        public DateTime StartLiving { get; private set; }
+
+        public int AverageTickRate { get; private set; }
+    }
+}
