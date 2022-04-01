c075bfdc3a9ce9a0593a4b93d5ad96b0ab03a935
Bert Tammenga <btammenga@pcugroup.com>
2022-04-01 17:21:33 +0200

    Timerpage added

--- BlazorStaticDemo/Program.cs
+++ BlazorStaticDemo/Program.cs
@@ -1,4 +1,5 @@
 using BlazorStaticDemo;
+using BlazorStaticDemo.Service;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddTransient<BlazorTimer>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ BlazorStaticDemo/Service/BlazorTimer.cs
@@ -0,0 +1,25 @@
+﻿//https://wellsb.com/csharp/aspnet/blazor-timer-navigate-programmatically
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace BlazorStaticDemo.Service;
+
+public class BlazorTimer
+{
+    private Timer _timer = new();
+
+    public void SetTimer(double interval)
+    {
+        _timer = new Timer(interval);
+        _timer.Elapsed += NotifyTimerElapsed;
+        _timer.Enabled = true;
+    }
+
+    public event Action OnElapsed;
+
+    private void NotifyTimerElapsed(Object source, ElapsedEventArgs e)
+    {
+        OnElapsed?.Invoke();
+        _timer.Dispose();
+    }
+}
